52bd028841f75fa80d639e66e7afdeb86303ad0e
Rafael Vieira <rafael.maluffe.vieira@gmail.com>
2021-09-28 12:05:18 -0300

    Flutter Tags no formuladio de adiçao de jogo

--- flutter/rec_game_app/lib/views/JogoAdd.dart
+++ flutter/rec_game_app/lib/views/JogoAdd.dart
@@ -1,4 +1,9 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+// best linha ever
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_tags/flutter_tags.dart';
 import 'package:intl/intl.dart';
 import 'package:rec_game_app/Service/JogoService.dart';
 
@@ -7,6 +12,8 @@ class JogoAdd extends StatelessWidget {
   final DateTime dateToday =
       DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
   final _form = GlobalKey<FormState>();
+  final GlobalKey<TagsState> _globalKey = GlobalKey<TagsState>();
+  final List GeneroTags = ['Plataforma', 'FPS', 'RPG', 'Puzzle'];
 
   @override
   Widget build(BuildContext context) {
@@ -33,39 +40,63 @@ class JogoAdd extends StatelessWidget {
               icon: Icon(Icons.save))
         ],
       ),
-      body: Form(
-        key: _form,
-        child: Column(
-          children: <Widget>[
-            Text("Nome:"),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Nome do Jogo"),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "O jogo tem que ter um Título";
-                }
-                if (value.trim().length <= 1) {
-                  return "Nome muito curto";
-                }
-              },
-              onSaved: (value) => descricao = value!,
-            ),
-            Text("Plataforma:"),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Escolha a plataforma"),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Tem que ter uma plataforma";
-                }
-              },
-              onSaved: (value) => descricao = value!,
-            ),
-            IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              Text("Nome:"),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Nome do Jogo"),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "O jogo tem que ter um Título";
+                  }
+                  if (value.trim().length <= 1) {
+                    return "Nome muito curto";
+                  }
                 },
-                icon: Icon(Icons.add_sharp))
-          ],
+                onSaved: (value) => descricao = value!,
+              ),
+              Text("Plataforma:"),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Escolha a plataforma"),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Tem que ter uma plataforma";
+                  }
+                },
+                onSaved: (value) => descricao = value!,
+              ),
+              Tags(
+                key: _globalKey,
+                itemCount: GeneroTags
+                    .length, //future lists parecem não ter metodo length build-in
+                columns: 6,
+                textField: TagsTextField(
+                    //onSubmitted: (String) ainda preciso do state funcionando pra isso
+                    ),
+                itemBuilder: (index) {
+                  final Item currentItem = GeneroTags[index];
+
+                  return ItemTags(
+                    index: index,
+                    title: GeneroTags[index],
+                    customData: currentItem,
+                    onPressed: (i) => print(i),
+                    onLongPressed: (i) => print(i),
+                    removeButton: ItemTagsRemoveButton(),
+                  );
+                },
+              ),
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.add_sharp))
+            ],
+          ),
         ),
       ),
     );
